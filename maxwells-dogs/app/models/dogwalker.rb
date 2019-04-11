052520bbb210084a7b186c731fe6a081e160ad4d
Elias Ventura <eliasventura23@gmail.com>
2019-04-11 00:27:05 +0000

    added model associations

--- maxwells-dogs/app/models/dogwalker.rb
+++ maxwells-dogs/app/models/dogwalker.rb
@@ -1,2 +1,4 @@
 class DogWalker < ApplicationController
+  has_many :owner_walks
+  has_many :walks through: :owner_walks
 end
---
+++ maxwells-dogs/app/models/owner_walks.rb
@@ -0,0 +1,4 @@
+class DogWalk < ActiveRecord::Base
+  belongs_to :owner
+  belongs_to :walk
+end
